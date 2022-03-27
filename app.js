e090bd6bdbd7431b457d5c59c9709bc8b6232976
Minju Kim <cpfl300@naver.com>
2022-03-27 17:49:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+// TODO: 배포전 삭제 임시변수
+***REMOVED***
+***REMOVED***
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
